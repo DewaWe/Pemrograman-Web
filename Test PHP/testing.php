8c2af45f2cef21fa3aa6e73c5542ed0d7b45276a
DewaWe <61811999+DewaWe@users.noreply.github.com>
2020-05-05 10:05:20 +0700

    Test PHP
    testing PHP localhost

---
+++ Test PHP/testing.php
@@ -0,0 +1,9 @@
+
+<h1><?php echo "Today is " . date("Y/m/d") . "<br>"; ?></h1>
+
+<?php echo '<h4>Nama : I Dewa Nyoman Gede Pramana Putra</h4>' ?>
+<?php echo '<h4>NIM : 1801010042</h4>' ?>
+<?php echo '<h4>Prodi : SI Pagi 2018</h4>' ?>
+
+<?php echo '<h3> Mencoba Belajar PHP untuk Pemrograman Web</h3>' ?>
+<br>
